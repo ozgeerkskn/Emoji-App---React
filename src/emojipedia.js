ef352bad47a8f48cf656aa794e3389fd2d0c7be6
ozgeerkskn <105421946+ozgeerkskn@users.noreply.github.com>
2023-12-26 13:37:12 +0300

    Add files via upload

---
+++ src/emojipedia.js
@@ -0,0 +1,60 @@
+const emojipedia = [
+  {
+    id: 1,
+    emoji: "💪",
+    name: "Tense Biceps",
+    meaning:
+      "“You can do that!” or “I feel strong!” Arm with tense biceps. Also used in connection with doing sports, e.g. at the gym.",
+  },
+  {
+    id: 2,
+    emoji: "🙏",
+    name: "Person With Folded Hands",
+    meaning:
+      "Two hands pressed together. Is currently very introverted, saying a prayer, or hoping for enlightenment. Is also used as a “high five” or to say thank you.",
+  },
+  {
+    id: 3,
+    emoji: "🤣",
+    name: "Rolling On The Floor, Laughing",
+    meaning:
+      "This is funny! A smiley face, rolling on the floor, laughing. The face is laughing boundlessly. The emoji version of “rofl“. Stands for „rolling on the floor, laughing“.",
+  },
+  {
+    id: 4,
+    emoji: "😜",
+    name: "Face With Stuck-Out Tongue & Winking Eye",
+    meaning:
+      "Characterized by a winking eye and stuck out tongue, conveys a cheeky and playful attitude that is ideal for humorous or ironic messages. The wink suggests a certain playfulness or flirtatiousness, making it perfect for situations where people are teasing or charming each other.",
+  },
+  {
+    id: 5,
+    emoji: "👻",
+    name: "Ghost",
+    meaning:
+      "The funny-smiling ghost is too cute to frighten others or cause harm. Symbol for Halloween or jokingly for creepy things, e.g. movies.",
+  },
+  {
+    id: 6,
+    emoji: "🥰",
+    name: "Smiling face with smiling eyes and three hearts",
+    meaning:
+      "This adorable emoji represents a smiling face surrounded by hearts, embodying infatuation and romance. It's perfect for expressing affection, love and romantic affairs of the heart while floating on cloud nine.",
+  },
+  {
+    id: 7,
+    emoji: "🤯",
+    name: "Face With Exploding Head",
+    meaning:
+      "I cannot believe it. or “That will blow you away.“ Something is very exciting or surprising. The exploding head can also be used to express shock or awe.",
+  },
+  {
+    id: 8,
+    emoji: "🙃",
+    name: "Upside-Down Face",
+    meaning:
+      "I'm not serious! The inverted smiling face indicates that you are not serious or have a humorous undertone. It is suitable for ambiguous statements, ironic remarks or joking comments. Also, it can be used to express silliness or in response to a joke.",
+  },
+];
+
+export default emojipedia;
